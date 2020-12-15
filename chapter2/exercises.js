d33eadad240f6418dbd058695bbe2d940f612efe
やた <tky@eno.local>
2020-12-15 20:51:24 +0900

    [Finish]6-2

---
+++ chapter2/exercises.js
@@ -0,0 +1,12 @@
+var int1 = 4
+var int2 = 8
+
+tasu = int1 + int2
+hiku = int1 - int2
+kakeru = int1 * int2
+waru = int1 / int2
+
+alert(int1 + "+" + int2 + "=" + tasu)
+alert(int1 + "-" + int2 + "=" + hiku)
+alert(int1 + "*" + int2 + "=" + kakeru)
+alert(int1 + "/" + int2 + "=" + waru)
